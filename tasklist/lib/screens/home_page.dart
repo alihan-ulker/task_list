b7a94fdb581d6e573d53fca9487617e798a2a71f
alihan-ulker <ali.ulker.217@gmail.com>
2021-12-13 00:49:16 +0300

    homepage ui update

--- tasklist/lib/screens/home_page.dart
+++ tasklist/lib/screens/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,13 +12,76 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: Text(
-          "Anasayfa",
-          style: TextStyle(color: Colors.white, fontSize: 32.0),
-        ),
+      body: ListView(
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: 80.0,
+                child: BackButton(
+                  onPressed: () {},
+                  color: Colors.white,
+                ),
+              ),
+              Positioned(
+                right: 20.0,
+                bottom: 10.0,
+                child: Container(
+                  width: 60.0,
+                  height: 60.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(60.0),
+                    border: Border.all(
+                      width: 2.0,
+                    ),
+                    image: const DecorationImage(
+                      image: NetworkImage(
+                          "https://cdn.pixabay.com/photo/2021/02/06/09/03/man-5987447_960_720.jpg"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: 100.0,
+            height: 100.0,
+            //color: Colors.blue,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "Task Lists",
+                    style: TextStyle(
+                      fontSize: 36.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Text(
+                    "4 Task",
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
